5e19a8e64c181612ada752a45603ab58c1ede68d
Courtney McKown <wheelerzac@yahoo.com>
2023-04-09 14:12:17 -0500

    added 1-5 answers in java

--- script.js
+++ script.js
@@ -31,19 +31,40 @@ answer: [
 {
 question: 'Who created the One ring??',
 answer: [
-        { text: 'Sauron', correct: false},
+        { text: 'Sauron', correct: true},
         { text: 'Frodo', correct: false},
-        { text: 'Tom Brady', correct: true},
-        { text: 'Alexander Dumas', correct: false},    
-
-
-
-
-
-
+        { text: 'Tom Brady', correct: false},
+        { text: 'Jeff Bezos', correct: false}, 
+]   
+},
+{
+    question: 'What was the name of the Mountain where the One ring was made?',
+    answer: [
+        { text: 'Mount Doom', correct: true},
+        { text: 'Mount Everest', correct: false},
+        { text: 'Backbone Mountain', correct: false},
+        { text: 'Mount Fury', correct: false},
+    ]
+    },
+    {
+        question: 'How many companions joined The "Fellowship of the Ring?',
+        answer: [
+            { text: 'Barack Obama', correct: false},
+            { text: 'Chef Boyardee', correct: false},
+            { text: 'J.R.R. Tolkien', correct: true},
+            { text: 'Alexander Dumas', correct: false},
+        ]
+        },
+    {
+        question: 'Who was Smeagols alter ego?',
+        answer: [
+            { text: 'Barack Obama', correct: false},
+            { text: 'Chef Boyardee', correct: false},
+            { text: 'J.R.R. Tolkien', correct: true},
+            { text: 'Alexander Dumas', correct: false},
+    ]
+    },
     
-}
-
 
 
 
