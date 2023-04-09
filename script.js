c18ffac3c1885f38828ad5c82b8c287b1cb32d35
Courtney McKown <wheelerzac@yahoo.com>
2023-04-09 14:01:44 -0500

    added more html

---
+++ script.js
@@ -0,0 +1,51 @@
+var quizContainer = document.getElementById("quiz-container");
+var questionContainer =
+var answerContainer =
+var timerElement = document.getElementById("timer");
+var submitButton = document.getElementById("submit-btn");
+
+//maybe add a var correctAnswer?
+
+var timeLeft = 30;
+var timerId;
+
+// function displayQuestion() {
+//         questions.forEach(question => {
+    //   question.style.display = 'none';
+//     });
+
+function startTimer() {
+    timerId =setInterval
+}
+
+var quizQuestions = [
+    {
+question: 'Who is the author of The Lord of the Rings?',
+answer: [
+    { text: 'Barack Obama', correct: false},
+    { text: 'Chef Boyardee', correct: false},
+    { text: 'J.R.R. Tolkien', correct: true},
+    { text: 'Alexander Dumas', correct: false},
+]
+},
+{
+question: 'Who created the One ring??',
+answer: [
+        { text: 'Sauron', correct: false},
+        { text: 'Frodo', correct: false},
+        { text: 'Tom Brady', correct: true},
+        { text: 'Alexander Dumas', correct: false},    
+
+
+
+
+
+
+    
+}
+
+
+
+
+
+];
