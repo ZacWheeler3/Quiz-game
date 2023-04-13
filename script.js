e05da04ecbb5814fe6cac949d90f033541e56276
Courtney McKown <wheelerzac@yahoo.com>
2023-04-12 23:26:05 -0500

    added to javascript and css

--- script.js
+++ script.js
@@ -1,102 +1,90 @@
-var startQuizButtonElement = document.getElementById("start-button");
-var questionContainerElement = document.getElementById("question-text");
-var answerContainerElement = document.getElementById("choice-buttons");
 var currentQuestion = 0;
-var timeRemaining = 30;
+var timeLeft = 30;
+var userScore = 0;
 
-var quizQuestions = [
+var questionsList = [
     {
-        questionText: 'Who is the author of The Lord of the Rings?',
-        choices: ['Barack Obama', 'Chef Boyardee', 'J.R.R Tolkien', 'Alexander Dumas'],
-        correctAnswer: 'J.R.R. Tolkien',
+        questionText: "Who is the author of The Lord of the Rings?",
+        choices: ["Barack Obama", "Chef Boyardee", "J.R.R. Tolkien", "Alexander Dumas"],
+        correctAnswer: "J.R.R. Tolkien",
     },
     {
-        questionText: 'Who created the One ring??',
-        choices: ['Sauron', 'Frodo', 'Tom Brady', 'Jeff Bezos'],
-        correctAnswer: 'Sauron',
+        questionText: "Who created the One ring?",
+        choices: ["Sauron", "Frodo", "Tom Brady", "Jeff Bezos"],
+        correctAnswer: "Sauron",
     },
     {
-        questionText: 'What was the name of the Mountain where the One ring was made?',
-        choices: ['Mount Doom', 'Mount Everest', 'Backbone Mountain', 'Mount Fury'],
-        correctAnswer: 'Mount Doom',
-
+        questionText: "What was the name of the Mountain where the One ring was forged?",
+        choices: ["Mount Doom", "Mount Everest", "Backbone Mountain", "Mount Fury"],
+        correctAnswer: "Mount Doom",
     },
     {
-        questionText: 'How many companions joined The "Fellowship of the Ring?',
-        choices: ['9', '3', '7', '14'],
-        correctAnswer: '9',
+        questionText: "How many companions joined The Fellowship of the Ring?",
+        choices: ["9", "3", "7", "14"],
+        correctAnswer: "9",
     },
     {
-        questionText: 'Who was Smeagols alter ego?',
-        choices: ['Luke Skywalker', 'Clark Kent', 'Santa Claus', 'Gollum'],
-        correctAnswer: 'Gollum',
-    },
-    {
-        questionText: 'What is the fake name Frodo Baggins used as he left the Shire?',
-        choices: ['GOAT', 'That Dude', 'Mr Underhill', 'Patrick Mahomes'],
-        correctAnswer: 'Mr Underhill',
-    },
-    {
-        questionText: 'What is the name of the pub in the Shire?',
-        choices: ['The Green Dragon', 'Pattys Pub', 'The Prancing Pony', 'McDonalds'],
-        correctAnswer: 'The Green Dragon',
-    },
-    {
-        questionText: 'How many rings of power are in existence?',
-        choices: ['1', '20', '4', '35'],
-        correctAnswer: '20',
-    },
-    {
-        questionText: 'Which character from the Lord of the Rings is NOT a hobbit?',
-        choices: ['Frodo Baggins', 'Gimli', 'Samwise Gamgee', 'Bilbo Baggins'],
-        correctAnswer: 'Gimli',
-    },
-    {
-        questionText: 'How many black riders are there?',
-        choices: ['6', '100', '5', '9'],
-        correctAnswer: '9',
-    },
+        questionText: "Who was Smeagol's alter ego?",
+        choices: ["Luke Skywalker", "Clark Kent", "Santa Claus", "Gollum"],
+        correctAnswer: "Gollum",
+    }
+    //   {
+    //     questionText: "What is the fake name Frodo Baggins used as he left the Shire?",
+    //     choices: ["GOAT", "That dude", "Mr. Underhill", "Patrick Mahomes"],
+    //     correctAnswer: "Mr. Underhill",
+    //   },
+    //   {
+    //     questionText: "What is the name of the pub in the Shire?",
+    //     choices: ["The Green Dragon", "Patty's Pub", "The Prancing Pony", "McDonald's"],
+    //     correctAnswer: "The Green Dragon",
+    //   },
+    //   {
+    //     questionText: "How many rings of power are in existence?",
+    //     choices: ["1", "20", "4", "35"],
+    //     correctAnswer: "20",
+    //   },
+    //   {
+    //     questionText: "Which character from the Lord of the Rings is <strong>NOT</strong> a hobbit?",
+    //     choices: ["Frodo Baggins", "Gimli", "Samwise Gamgee", "Bilbo Baggins"],
+    //     correctAnswer: "Gimli",
+    //   },
+    //   {
+    //     questionText: "How many black riders are there?",
+    //     choices: ["6", "100", "5", "9"],
+    //     correctAnswer: "9",
+    //   }
 ];
 
-// function startQuiz() {
-//     console.log("I click the button");
-// };
-console.log(startQuizButtonElement);
+var startQuizButtonElement = document.getElementById("start-button");
 startQuizButtonElement.addEventListener("click", startQuiz);
 
 function startQuiz() {
     console.log("I click the button");
     startQuizButtonElement.style.display = "none";
-    // createQuizStage();
-    createQuestionElements(quizQuestions[currentQuestion]);
-   var timerInterval = setInterval(function() {
-        timeRemaining--;
-        document.getElementById("timer-container").innerHTML = "Time left: " + timeRemaining;
-        if (timeRemaining <= 0) {
-            clearInterval(timerInterval);
-            var alertTimeout = setTimeout(function() {
-                alert("Time's up!");
-                document.getElementById("timer-container").style.display = "none";
-                currentQuestion++; // increment currentQuestion to progress to the next question
-                if (currentQuestion < quizQuestions.length) {
-                    createQuestionElements(quizQuestions[currentQuestion]);
-                } else {
-                    // quiz is over
-                }
-            }, 1000);
+    createQuestionElements(questionsList[currentQuestion]);
+    var timeOut = setInterval(function () {
+        timeLeft--;
+        document.getElementById("timer-container").innerHTML = "Time left: " + timeLeft;
+        if (timeLeft <= 0) {
+            clearInterval(timeOut);
+            alert("Time's up!");
         }
     }, 1000);
 }
-
 function createQuizStage() {
     var quizContainer = document.createElement("article");
     quizContainer.classList.add("quiz-container");
     document.body.appendChild(quizContainer);
 }
 function createQuestionElements(currentQuestionData) {
+    // Remove any existing question elements
+    var quizContainer = document.querySelector(".quiz-container");
+    quizContainer.innerHTML = "";
+
+    // Create elements for the current question
     var currentQuestionText = currentQuestionData.questionText;
     var questionTextBox = document.createElement("h3");
-    var choiceOne = document.createElement("button", "click", checkAnswer);
+    var choiceOne = document.createElement("button");
     var choiceTwo = document.createElement("button");
     var choiceThree = document.createElement("button");
     var choiceFour = document.createElement("button");
@@ -106,36 +94,50 @@ function createQuestionElements(currentQuestionData) {
     choiceTwo.textContent = currentQuestionData.choices[1];
     choiceThree.textContent = currentQuestionData.choices[2];
     choiceFour.textContent = currentQuestionData.choices[3];
-    var quizContainer = document.querySelector(".quiz-container").appendChild(questionTextBox);
+    quizContainer.appendChild(questionTextBox);
     quizContainer.appendChild(choiceContainer);
     choiceContainer.appendChild(choiceOne);
     choiceContainer.appendChild(choiceTwo);
     choiceContainer.appendChild(choiceThree);
     choiceContainer.appendChild(choiceFour);
-};
-// questionContainerElement.textContent = quizQuestions[0].questionText;
 
-for (var i = 0; i < quizQuestions[0].choices.length; i++) {
-    var newChoiceButton = document.createElement("button");
-    newChoiceButton.textContent = quizQuestions[0].choices[i];
-    choiceContainer.appendChild(newChoiceButton);
-};
-function createQuestionElements(currentQuestionData) {
-    // code to create question and answer choice elements
-    choiceOne.addEventListener("click", checkAnswer);
-    choiceTwo.addEventListener("click", checkAnswer);
-    choiceThree.addEventListener("click", checkAnswer);
-    choiceFour.addEventListener("click", checkAnswer);
-  
-    function checkAnswer(event) {
-      var selectedAnswer = event.target.textContent;
-      if (selectedAnswer === currentQuestionData.correctAnswer) {
-        // display "correct" message
-      } else {
-        // display "incorrect" message with correct answer
-      }
+    choiceOne.addEventListener("click", function () {
+        checkAnswer(choiceOne, currentQuestionData);
+    });
+    choiceTwo.addEventListener("click", function () {
+        checkAnswer(choiceTwo, currentQuestionData);
+    });
+    choiceThree.addEventListener("click", function () {
+        checkAnswer(choiceThree, currentQuestionData);
+    });
+    choiceFour.addEventListener("click", function () {
+        checkAnswer(choiceFour, currentQuestionData);
+    });
+}
+function checkAnswer(selectedChoice, currentQuestionData) {
+    if (selectedChoice.textContent === currentQuestionData.correctAnswer) {
+        console.log("Correct answer");
+        userScore += 20;
     }
-  }
+    currentQuestion++;
+    if (currentQuestion < questionsList.length) {
+        createQuestionElements(questionsList[currentQuestion]);
+    } else {
+        console.log("Quiz completed");
+        displayScore();
+    }
+}
+function displayScore() {
+    var initials = prompt("Congratulations! You finished with a score of " + userScore + ". Please enter your initials to save your score:");
+    localStorage.setItem(initials, userScore);
+    window.location.href = "highscores.html";
+};
+const highScore = localStorage.getItem('highScore');
+const highScorePlayer = localStorage.getItem('highScorePlayer');
+
+document.getElementById('high-score').textContent = highScore;
+document.getElementById('high-score-player').textContent = highScorePlayer;
+
 
 
 
